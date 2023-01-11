cb29ed98640837a79c484d803bcf6e328f21f36f
Tyler Bedard <tylerbedard126@gmail.com>
2023-01-10 20:18:05 -0500

    Ingredients model set up?

---
+++ models/Ingredients.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Ingredients extends Model {}
+
+Ingredients.init(
+    {
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNulll: false,
+    },
+}, 
+{
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'ingredients',
+}
+);
+
+
+
+
+module.exports = Ingredients;
