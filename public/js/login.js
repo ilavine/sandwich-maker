83459a1c05757db22c23abf030be5e22665b0406
AStandish <112442942+AStandish@users.noreply.github.com>
2023-01-19 19:54:49 -0500

    added code to login

--- public/js/login.js
+++ public/js/login.js
@@ -25,3 +25,7 @@ const loginFormHandler = async (event) => {
 document
   .querySelector('.login-form')
   .addEventListener('submit', loginFormHandler);
+
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
