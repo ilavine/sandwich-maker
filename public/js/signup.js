8cc6f2ba9074ee8e7cadae5f9c02adad679dcbb3
Tyler Bedard <tylerbedard126@gmail.com>
2023-01-19 20:47:14 -0500

    signup works

---
+++ public/js/signup.js
@@ -0,0 +1,24 @@
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#new_user_name').value.trim();
+  const email = document.querySelector('#new_email').value.trim();
+  const password = document.querySelector('#new_password').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/sandwich');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+document
+  .querySelector('.signup-form')
+  .addEventListener('submit', signupFormHandler);
