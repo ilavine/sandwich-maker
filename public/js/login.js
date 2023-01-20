d6469174762ba8ec6c8f08537bb25eb99209a70f
AStandish <112442942+AStandish@users.noreply.github.com>
2023-01-19 20:11:49 -0500

    combined login and signup

--- public/js/login.js
+++ public/js/login.js
@@ -2,8 +2,8 @@ const loginFormHandler = async (event) => {
   event.preventDefault();
 
   // Collect values from the login form
-  const email = document.querySelector('#email').value.trim();
-  const password = document.querySelector('#password').value.trim();
+  const email = document.querySelector('#email-login').value.trim();
+  const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
     // Send a POST request to the API endpoint
@@ -12,10 +12,32 @@ const loginFormHandler = async (event) => {
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-    console.log(response);
+
     if (response.ok) {
-      // If successful, redirect the browser to the homepage but with a logout button instead of login
-      document.location.replace('/dashboard');
+      // If successful, redirect the browser to the profile page
+      document.location.replace('/sandwich');
+    } else {
+      alert(response.statusText);
+    }
+  }
+};
+
+const signupFormHandler = async (event) => {
+  event.preventDefault();
+
+  const name = document.querySelector('#name-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
+
+  if (name && email && password) {
+    const response = await fetch('/api/users', {
+      method: 'POST',
+      body: JSON.stringify({ name, email, password }),
+      headers: { 'Content-Type': 'application/json' },
+    });
+
+    if (response.ok) {
+      document.location.replace('/sandwich');
     } else {
       alert(response.statusText);
     }
