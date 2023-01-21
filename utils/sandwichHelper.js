6264f9b1531fd63b6f062975243206e9fe331945
Tyler Bedard <tylerbedard126@gmail.com>
2023-01-21 12:29:43 -0500

    login, signup and logout all work

--- utils/sandwichHelper.js
+++ utils/sandwichHelper.js
@@ -1,24 +1,24 @@
 // array of sandwich objects for the slides
 
 // Fetch the data for the carousel
-function fetchCarouselData() {
-  // Example data
-  const data = [
-    {
-      title: "Slide 1",
-      description: "This is the first slide",
-      image: "path/to/image1.jpg"
-    },
-    {
-      title: "Slide 2",
-      description: "This is the second slide",
-      image: "path/to/image2.jpg"
-    },
-    {
-      title: "Slide 3",
-      description: "This is the third slide",
-      image: "path/to/image3.jpg"
-    }
-  ];
-  return data;
-}
+// function fetchCarouselData() {
+//   // Example data
+//   const data = [
+//     {
+//       title: 'Slide 1',
+//       description: 'This is the first slide',
+//       image: 'path/to/image1.jpg',
+//     },
+//     {
+//       title: 'Slide 2',
+//       description: 'This is the second slide',
+//       image: 'path/to/image2.jpg',
+//     },
+//     {
+//       title: 'Slide 3',
+//       description: 'This is the third slide',
+//       image: 'path/to/image3.jpg',
+//     },
+//   ];
+//   return data;
+// }
