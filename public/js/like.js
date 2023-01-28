d42062ec034d2ead0e423f5e784df31b8d708eae
Iuliia Lavine <iulii@MacBook-Pro.hitronhub.home>
2023-01-28 00:06:15 -0500

    event delegation for like btn, working on frontend only

--- public/js/like.js
+++ public/js/like.js
@@ -1,7 +1,33 @@
-$('.btn-like').click(function () {
-  $('.label').html(function (i, val) {
-    return val * 1 + 1;
-  });
-});
+const likeButtons = $('.btn-like');
+let clicked = false;
 
+const clickHandler = function () {
+  let count = 0;
+  return function () {
+    if (!clicked) {
+      clicked = true;
+      count++;
+      this.innerHTML = `<i class='fa-solid fa-heart'> </i> ${count}`;
+    } else {
+      clicked = false;
+      count--;
+      this.innerHTML = `<i class='fa-regular fa-heart'> </i> ${count}`;
+    }
+    // const response = fetch('/api/sandwich/', {
+    //   method: 'POST',
+    //   body: JSON.stringify({ likeCount: count }),
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //   },
+    // });
+    // if (response.ok) {
+    //   document.location.reload();
+    // } else {
+    //   console.log(response);
+    // }
+  };
+};
 
+for (let i = 0; i < likeButtons.length; i++) {
+  likeButtons[i].addEventListener('click', clickHandler());
+}
